7cce4679fc3b101c917696d39c41f73b9b872d44
betaluis <luis.betancourt.dev@gmail.com>
2022-11-04 23:30:01 -0500

    update website info

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,17 @@
-import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from 'astro/config'
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
-import react from "@astrojs/react";
+import react from '@astrojs/react'
 
 // https://astro.build/config
-import image from "@astrojs/image";
+import image from '@astrojs/image'
 
 // https://astro.build/config
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
   integrations: [sitemap(), react(), image(), tailwind()]
-});
+})
--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'What I Do - Luis Betancourt'
+export const SITE_DESCRIPTION = 'A currated list of what I use in my workflow'
+export const HOMEPAGE_URL = 'https:www.luisbetancourt.dev'
